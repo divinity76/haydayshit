89a5438224032087fe38b1421b947db6975fbcb4
divinity76 <divinity76@gmail.com>
2014-10-22 02:37:52 +0200

    code to fake friends

---
+++ fakefriends.js
@@ -0,0 +1,7 @@
+		if(oSession.uriContains("https://graph.facebook.com/me/friends?sdk=ios&fields=id%2Cname%2Cpicture%2Cfirst_name%2Cgender%2Clocale%2Cusername%2Cinstalled&")){
+		            oSession["ui-backcolor"] = "yellow";
+					      oSession.hostname="raw.githubusercontent.com";
+						  oSession.PathAndQuery="/divinity76/haydayshit/master/2000_GraphApiHayDayPlayerResponse.json"
+
+//https://raw.githubusercontent.com/divinity76/haydayshit/master/2000_GraphApiHayDayPlayerResponse.json
+		}
