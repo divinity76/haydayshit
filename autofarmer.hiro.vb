d0446b11934e7484ba48714cedb1a53bf7af48a9
divinity76 <divinity76@gmail.com>
2014-11-24 20:07:11 +0100

    Create autofarmer.hiro.vb

---
+++ autofarmer.hiro.vb
@@ -0,0 +1,21 @@
+//<InlineFuncGoToLeftTopOfSCreen1>
+//goes to top left of screen, from anywhere on the map..
+touchDown 0 548 177
+sleep 56
+touchMove 0 547 180
+sleep 12
+touchMove 0 534 201
+sleep 14
+touchMove 0 500 327
+sleep 11
+touchMove 0 447 578
+sleep 13
+touchMove 0 363 867
+sleep 19
+touchMove 0 260 1158
+sleep 9
+touchUp 0
+//</InlineFuncGoToLeftTopOfSCreen1>
+
+//<InlineFuncSeedWheat1>
+//seed wheat on ALL fields... can be optimized to be much faster 
